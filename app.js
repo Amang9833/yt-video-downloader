2ee9048530272f13ee029a991272a53844339e1c
aman <amang9833@gmail.com>
2022-07-19 23:33:26 +0530

    api for yt vieo download

---
+++ app.js
@@ -0,0 +1,38 @@
+const express = require('express')
+const cors = require('cors')
+const app = express();
+
+const scrapeUrl = require('./index')
+
+app.use(express.json())
+
+
+app.use(cors())
+
+const callMe = (sta) => {
+    
+    app.get('/',async (req, res) => {
+        let data = await scrapeUrl();
+        console.log('get method -> ', data)
+        console.log(sta)
+        res.send(`<a href=${data}>click me to download</a>`)
+        
+    })
+}
+
+callMe("https://youtu.be/dXjKh66BR2U");
+
+app.post('/ytUrl', async (req, res) => {
+    const url = await req.body.url;
+    let toDelviery =  await scrapeUrl(url);
+    callMe(toDelviery);
+//    res.status(200).send(`<a href=${toDelviery}>click me to download</a>`);
+   res.status(200).send(toDelviery);
+
+})
+
+
+
+
+
+app.listen(80 || process.env.PORT, () => { console.log('backend is running at port 80') });
---
+++ index.js
@@ -0,0 +1,55 @@
+const puppeteer = require("puppeteer");
+
+// ------------> for debug Monitor console
+
+const url = "https://yt1s.com/en342"; // --> always fix
+let answer;
+
+module.exports = async function scrapeUrl(userUrl) {
+  // let yt_url = "https://youtu.be/dXjKh66BR2U";
+  let yt_url = userUrl;
+  // let yt_url = userUrl;
+
+  const browser = await puppeteer.launch({ headless: true }); // launch the browser
+  const page = await browser.newPage(); // create a new instance
+  await page.goto(url, { waitUntil: "domcontentloaded" }); // get into that website
+
+  // pastting url website
+
+  // console.log("1 visited website");
+
+  await page.type("[name=q]", yt_url); // paste url
+
+  // console.log("2 paste the url");
+
+  await page.click("[type=button]");
+
+  // console.log("3 clicked the button");
+
+  //page to convert the yt to downloadeable file
+  await page.waitForTimeout(2000);
+
+  await page.click("[id='btn-action']");
+  await page.waitForTimeout(2000);
+
+  // console.log("4 witing for final url");
+
+  // console.log("abhi ruuk");
+  let finalUrl;
+
+  const getData = async () => {
+    return await page.evaluate(async () => {
+      finalUrl = document.getElementById("asuccess").href;
+      return await finalUrl;
+    });
+  };
+
+  answer = await getData();
+  // console.log("res -> ", answer);
+
+  // console.log("hogya");
+
+  await browser.close();
+
+  return answer;
+};
